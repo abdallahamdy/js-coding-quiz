d04b003aaba86335247f76b6a9df54d55a34d99f
AHamdy <abdulla.mh@hotmail.com>
2022-06-23 19:39:46 -0400

    added styling and content for the questions pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
+var pageContentEl = document.querySelector(".page-content");
 var quizBtn = document.querySelector("#start-quiz-btn");
-var introPageEl = document.querySelector(".intro-page");
+var introPageEl = document.querySelector(".main-page");
 
-var displayQuiz = function(){
+var displayQuizQuestions = function(){
 
+    var questionPageEl = document.createElement("div");
+    questionPageEl.className = "question-page";
     
+    var questionEl = document.createElement("h1");
+    questionEl.className = "the-question";
+    questionEl.textContent = "7ayak allah a5ooy"
+
+    questionPageEl.appendChild(questionEl);
+
+    pageContentEl.appendChild(questionPageEl);
 
 }
 
 var startQuizHandler = function(){
     console.log("HELLO");
-    introPageEl.remove();
-    displayQuiz();
+    introPageEl.style.display = 'none';
+    displayQuizQuestions();
+    // introPageEl.style.display = 'flex';
 }
 
 quizBtn.addEventListener("click", startQuizHandler);
